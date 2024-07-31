ce3e3036b4e36073de407947ab04995e7a7f2c83
Toasty <68469450+HeatedBread@users.noreply.github.com>
2024-07-30 22:08:10 -0500

    feat(home-page): implemented a homepage

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div className="page home-page">
-        Home
+      <h1 className="home-page__title">
+        Inspiring Next-Generation Innovators Through Ingenious Advancements
+      </h1>
+
+      <div className="home-page__banner">
+        <div className="home-page__banner-left">
+          <img src="./InitiaShowcase.png" alt="" className='home-page__image'/>
+        </div>
+        <div className="home-page__banner-right">
+          <h2 className="home-page__banner-title">Who Are We?</h2>
+          <p className="home-page__banner-text">
+            Initia is a non-profit organization striving to make change in our community. We host recurring competitions that aim to solve problems around us, which are catered towards various fields from engineering, to medical, to business. We ensure that each person is given the opportunity to contribute their skill sets to Initia's movement. 
+          </p>
+          <Link to='/join' className="home-page__banner-btn">Explore Open Competitions</Link>
+        </div>
+      </div>
+
     </div>
   )
 }
 
-export default Home
+export default Home;
--- src/routes/Solutions.jsx
+++ src/routes/Solutions.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
+const solutions = [
+  {
+    title: "",
+    card1: {
+      name: "Resistive Conductor Adaptors",
+      authors: "John Fahoum & David Nguyen",
+      school: "Carroll High School"
+    }
+  }
+]
+
 const Solutions = () => {
   return (
-    <div className="page solutions-page"></div>
+    <div className="page solutions-page">
+      {
+        // solutions.map()
+      }
+    </div>
   )
 }
 
